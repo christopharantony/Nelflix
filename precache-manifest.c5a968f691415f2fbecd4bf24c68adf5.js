e78a070514374004f6a2d8a94669ab56b1902976
Christophar Antony <99424113+christopharantony@users.noreply.github.com>
2023-10-02 18:16:44 +0530

    Add files via upload

---
+++ precache-manifest.c5a968f691415f2fbecd4bf24c68adf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a0cba0b4eee794af67c",
+    "url": "/static/css/main.c0a29411.chunk.css"
+  },
+  {
+    "revision": "3a0cba0b4eee794af67c",
+    "url": "/static/js/main.3a0cba0b.chunk.js"
+  },
+  {
+    "revision": "02da862bfea02081f885",
+    "url": "/static/js/1.02da862b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2395d9a2fdea9a1ff93303696a845118",
+    "url": "/index.html"
+  }
+];
